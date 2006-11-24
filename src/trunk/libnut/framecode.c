0c300a7783524ca38c5dc1350820a3ef84036729
ods15 <ods15@f74bf30e-522f-0410-a534-925f68dc5a93>
2006-11-24 07:24:00 +0000

    Small fix for framecode consume generation
    git-svn-id: svn://svn.mplayerhq.hu/nut@459 f74bf30e-522f-0410-a534-925f68dc5a93

--- src/trunk/libnut/framecode.c
+++ src/trunk/libnut/framecode.c
@@ -82,8 +82,10 @@ void nut_framecode_generate(const nut_stream_header_t s[], nut_frame_table_input
 	}
 
 	for (i = 0; i < stream_count; i++) if (consume[i]) tot_con++;
+
+	if (tot_con) tot_con = (254 - (n+m))/tot_con; // 256 - 'N' - 0xFF invalids
 	if (tot_con) for (i = 0; i < stream_count; i++) {
-		int al = (254 - (n+m))/tot_con; // 256 - 'N' - 0xFF invalids
+		int al = tot_con;
 		switch (consume[i]) {
 		case e_consume_none:
 			break;
